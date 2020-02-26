be2166c0786ac578c8bb75224ea5b5848eb244cb
andrew-levy <alevy97@gmail.com>
2020-02-25 19:25:13 -0500

    updated styles, responsive mobile behavior

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,12 +4,12 @@ import me from '../images/me.jpg';
 const About = () => {
     return (
       <div id="About" className="about-container" id="About">
-        <img className="me" src={me} alt="" width="200px" />
+        <img className="me" src={me} alt="" />
         <div className="text-area">
           <h2 className="text large collapse-bottom">Andrew Levy</h2>
           <p className="text medium"> Mathematician/Software Developer</p>
           <p className="text small"> 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            I recently graduated from James Madison University ('19) with a degree in Mathematics and Computer Science. I am currently a Software Developer - Consultant at CapTech Ventures, Inc. Some of my interests include pure mathematics, virtual reality, augmented reality, web and mobile development. 
           </p>
         </div>
       </div>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,6 +4,14 @@ const Contact = () => {
     return (
       <div id="Contact" className="contact-container" id="Contact">
         <h2 className="text large">Contact Me</h2>
+        <ul className="contact-list">
+            <li className="contact-bar">
+                item
+            </li>
+            <li className="contact-bar">
+                item
+            </li>
+        </ul>
       </div>
     );
   }
